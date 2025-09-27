68db204e6dda23d8bf0df76c670788a7a36976fa
kazedevs <businessrajkumardas@gmail.com>
2025-09-27 09:50:06 +0530

    context-api and project 6 (themeswitcher)

---
+++ project-6/src/context/theme.js
@@ -0,0 +1,13 @@
+import React, { createContext, useContext } from "react";
+
+export const Themecontext = createContext({
+  themeMode: "light",
+  darkTheme: () => {},
+  lightTheme: () => {},
+});
+
+export const ThemeProvider = Themecontext.Provider;
+
+export default function useTheme() {
+  return useContext(Themecontext);
+}
