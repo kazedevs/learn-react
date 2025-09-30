b22fa3962faf602cfd55197d99d2f5123c3ac648
kazedevs <businessrajkumardas@gmail.com>
2025-09-30 12:49:24 +0530

    todo project with context pai and localstorage

---
+++ project-7/src/context/TodoContext.js
@@ -0,0 +1,23 @@
+import { useContext, createContext } from "react";
+
+
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: Date.now(),
+            todo: "Todo msg",
+            completed: false
+        }
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+});
+
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider
